d27d3c1135c64cff5987d42225c45edc1785471f
Yaritza <yaritzamicaelleb@gmail.com>
2021-03-26 19:57:49 -0300

    Atividade Idade Aluno

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace IdadeAluno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime aniversario = new DateTime(2005, 03, 28);
+            DateTime anv = new DateTime(2023, 03, 28);
+
+            string datalonga= aniversario.ToLongDateString();
+            string datalonga2= anv.ToLongDateString();
+
+            Console.WriteLine($"Eu nasci em: {datalonga}");
+            Console.WriteLine($"Farei 18 anos em: {datalonga2}");
+        }
+    }
+}
